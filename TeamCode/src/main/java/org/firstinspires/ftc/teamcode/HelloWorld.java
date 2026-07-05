00283dab2791ab6c8d7704b03969bee3f2db0797
TeamClawlers <fusrobotics25@gmail.com>
2026-07-05 21:08:36 +0100

    Second part of the first java class for Ftc.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @Disabled
-@Autonomous
+@TeleOp
 public class HelloWorld extends OpMode {
 
     @Override
